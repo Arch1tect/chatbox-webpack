8b46e44dc6737b8a7e82c932516a5697cbfe7519
ArchITech <swtdavid@gmail.com>
2019-02-28 01:19:27 -0800

    block ad request for duonao

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -4,3 +4,12 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     }
 
 });
+console.log(1234567)
+chrome.webRequest.onBeforeRequest.addListener(
+  function(details) {
+  	console.log(details);
+    return {cancel: details.url.indexOf("s1-a4.dnvod.tv") != -1};
+  },
+  { urls: ["<all_urls>"] },
+  ["blocking"]
+);
