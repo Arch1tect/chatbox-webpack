7955d102a1139940f66291bd30e869c48d5d3612
ArchITech <swtdavid@gmail.com>
2019-05-27 00:20:09 -0700

    v4.0.1

---
+++ chrome-extension/build/precache-manifest.0e29594e6e411de64ee273b92e036f34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "./static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "c997702548ae3883c305",
+    "url": "./static/js/main.0e932fe8.chunk.js"
+  },
+  {
+    "revision": "0e8c9f30485872e7e4ad",
+    "url": "./static/js/2.4b19c3dc.chunk.js"
+  },
+  {
+    "revision": "c997702548ae3883c305",
+    "url": "./static/css/main.b885a163.chunk.css"
+  },
+  {
+    "revision": "0e8c9f30485872e7e4ad",
+    "url": "./static/css/2.5b6f3ec6.chunk.css"
+  },
+  {
+    "revision": "2218beb7749f0d032fb138b8742166b8",
+    "url": "./index.html"
+  }
+];
--- chrome-extension/chatbox-only/content.js
+++
@@ -1,8 +0,0 @@
-if (location.href.indexOf('dnvod') > -1) {
-	var duonaoScript = document.createElement('script');
-	duonaoScript.src = chrome.runtime.getURL('duonao.js');
-	duonaoScript.onload = function() {
-	    duonaoScript.remove();
-	};
-	document.body.appendChild(duonaoScript);
-}
